b66488ea6500410ed17d17b0c5bc9755216ae2ea
Ilya D <ilya_dt@protonmail.com>
2024-12-23 17:29:09 +0300

    корректировка отображение даты

--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -12,8 +12,21 @@ const dateStart = new Date(budget.dateFrom);
 const dateEnd = new Date(budget.dateTo);
 const daysCount = (dateEnd-dateStart) / 1000 / 60 / 60 / 24 + 1
 
-function dateFormat(i) {
-  let d = new Date(dateStart.getTime() + (i-1)*24*60*60*1000);
+function getDate(i) {
+  return new Date(dateStart.getTime() + (i-1)*24*60*60*1000);
+}
+
+// https://stackoverflow.com/a/2998822/15347300
+function pad(num, size) {
+    var s = "00" + num;
+    return s.substring(s.length-size);
+}
+
+function dateFormat(d) {
+  return pad(d.getDate(), 2)  + "." + pad(d.getMonth()+1, 2) + "." + d.getFullYear();
+}
+
+function dateISO(d) {
   return d.toISOString().split('T')[0];
 }
 
@@ -27,9 +40,9 @@ function dateFormat(i) {
         <b>{{ budget.dateFrom }} &mdash; {{ budget.dateTo }}</b>
     </p>
   </div>
-  <div v-for="i in daysCount" :set="day = dateFormat(i)" class="row">
-    <p><i>{{ day }}</i></p>
-    <DaySpendingTable :daySpendings="props.spendings[day]"/>
+  <div v-for="i in daysCount" :set="date = getDate(i)" class="row">
+    <p><i>{{ dateFormat(date) }}</i></p>
+    <DaySpendingTable :daySpendings="props.spendings[dateISO(date)]"/>
     <p></p>
   </div>
 </template>
