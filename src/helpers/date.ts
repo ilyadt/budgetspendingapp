0482d4fd4be57e259fecc4bcfa9d124c3e1de883
Ilya D <ilya_dt@protonmail.com>
2025-01-03 15:19:23 +0300

    small typescript

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,25 +1,25 @@
 
 // https://stackoverflow.com/a/2998822/15347300
-function pad(num, size) {
+function pad(num: number, size: number): string {
   var s = "00" + num;
   return s.substring(s.length-size);
 }
 
-export function dateFormat(d) {
+export function dateFormat(d: Date): string {
   return pad(d.getDate(), 2)  + "." + pad(d.getMonth()+1, 2) + "." + d.getFullYear();
 }
 
-export function dateFormatFromString(dStr) {
+export function dateFormatFromString(dStr: string): string {
   let d = new Date(dStr);
 
   return dateFormat(d);
 }
 
-export function dateISO(d) {
+export function dateISO(d: Date): string {
   return d.toISOString().split('T')[0];
 }
 
-export function dateISOFromString(dStr) {
+export function dateISOFromString(dStr: string): string {
   let d = new Date(dStr);
 
   return dateISO(d);
