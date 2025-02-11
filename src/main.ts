90de41aa8a6006b545e8ecb720d3ec2e67397562
Ilya D <ilya_dt@protonmail.com>
2025-02-11 15:15:36 +0300

    hash history

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia, storeToRefs } from 'pinia'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useBudgetSpendingsStore } from './stores/budgetSpendings'
 import BudgetView from './views/BudgetView.vue'
 
@@ -12,7 +12,8 @@ app.use(pinia)
 
 app.use(
   createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    // Используется HashHistory, потому что статический сервер не использует url-rewrite-rules
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
       {
         path: '/',
@@ -20,7 +21,7 @@ app.use(
           const store = useBudgetSpendingsStore()
           const { budgets } = storeToRefs(store)
 
-          return { name: 'budget', params: { budgetId: budgets.value[0].id } }
+          return { name: 'budget', params: { budgetId: budgets.value[0].id }, query: {a:"b"} }
         },
       },
       {
