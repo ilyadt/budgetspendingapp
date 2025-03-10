defd1c5899160520f9834a654d5e32a363c58cbe
Ilya D <ilya_dt@protonmail.com>
2025-03-10 13:53:52 +0300

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const baseUrl = env.VITE_BASE_URL || ''
 
   return {
-    base: env.VITE_BASE_URL,
+    base: baseUrl,
     plugins: [
       vue(),
       vueJsx(),
@@ -20,21 +22,27 @@ export default defineConfig(({mode}) => {
       VitePWA({
         registerType: 'autoUpdate',
         workbox: {
-          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         },
         manifest: {
           theme_color: '#ffffff',
-        }
+          icons: [
+            { src: baseUrl + '/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+            { src: baseUrl + '/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+            { src: baseUrl + '/icons/apple-touch-icon.png', type: 'image/png' },
+          ],
+        },
       }),
       sentryVitePlugin({
-        org: "dim-2a",
-        project: "go",
+        org: 'dim-2a',
+        project: 'go',
         authToken: env.VITE_SENTRY_AUTH_TOKEN,
       }),
     ],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-}})
+  }
+})
