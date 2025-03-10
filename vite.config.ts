9e4aee591b80515c96aea8153114a0db017016cc
Ilya D <ilya_dt@protonmail.com>
2025-03-10 12:40:53 +0300

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: env.BASE_URL,
+    base: env.VITE_BASE_URL,
     plugins: [
       vue(),
       vueJsx(),
